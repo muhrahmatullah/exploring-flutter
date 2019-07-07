849d98cd30bd1a758f3cd756f07f340f94cb0f70
Muh Rahmatullah <muhammadrahmatullah1@gmail.com>
2019-07-07 19:55:49 +0700

    finish task navigation

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -4,11 +4,9 @@
 
 import 'package:flutter/material.dart';
 
-// TODO: Check if we need to import anything
-
 import 'package:flutter_app_sample/category.dart';
+import 'package:flutter_app_sample/unit.dart';
 
-// TODO: Define any constants
 final _backgroundColor = Colors.green[100];
 
 /// Category Route (screen).
@@ -43,26 +41,44 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  /// Makes the correct number of rows for the list view.
+  ///
+  /// For portrait, we use a [ListView].
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
+  /// Returns a list of mock [Unit]s.
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Create a list of the eight Categories, using the names and colors
-    // from above. Use a placeholder icon, such as `Icons.cake` for each
-    // Category. We'll add custom icons later.
-
     final categories = <Category>[];
 
     for (var i = 0; i < _categoryNames.length; i++) {
       categories.add(Category(
         name: _categoryNames[i],
         color: _baseColors[i],
-        icon: Icons.cake,
+        iconLocation: Icons.cake,
+        units: _retrieveUnitList(_categoryNames[i]),
       ));
     }
 
     final listView = Container(
       color: _backgroundColor,
       padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: _buildCategoryWidgets(categories, context),
+      child: _buildCategoryWidgets(categories),
     );
 
     final appBar = AppBar(
@@ -82,27 +98,5 @@ class CategoryRoute extends StatelessWidget {
       appBar: appBar,
       body: listView,
     );
-  }
-
-  /// Makes the correct number of rows for the list view.
-  ///
-  /// For portrait, we construct a [ListView] from the list of category widgets.
-  /// For landscape, we construct a [GridView] with two collumn
-  ///
-  /// To check the device orientation we use MediaQuery which need context, thats
-  /// why we passed extra param context for it
-  Widget _buildCategoryWidgets(List<Widget> categories, BuildContext context) {
-    if (MediaQuery.of(context).orientation == Orientation.portrait) {
-      return ListView.builder(
-        itemBuilder: (BuildContext context, int index) => categories[index],
-        itemCount: categories.length,
-      );
-    } else {
-      return GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: 3,
-        children: categories,
-      );
-    }
   }
 }
